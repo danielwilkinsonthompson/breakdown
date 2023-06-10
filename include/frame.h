a6810ec9a1905935118daaf13209309007cfdc90
Daniel Wilkinson-Thompson <daniel@wilkinson-thompson.com>
2023-06-11 02:11:13 +1000

    fixed frame.h formatting

--- include/frame.h
+++ include/frame.h
@@ -5,6 +5,7 @@ frame compositor
 
 © Daniel Wilkinson-Thompson 2023
 daniel@wilkinson-thompson.com
+
 references:
  - https://github.com/danielwilkinsonthompson/breakdown
  - https://github.com/emoon/minifb
@@ -13,5 +14,11 @@ references:
 #define __frame_h
 #include <stdint.h> // type definitions
 
-void frame_msleep(uint32_t microseconds);
+/*----------------------------------------------------------------------------
+  msleep
+  ----------------------------------------------------------------------------
+  sleep current thread
+  ms :  number of milliseconds to put thread to sleep
+-----------------------------------------------------------------------------*/
+void frame_msleep(uint32_t ms);
 #endif
