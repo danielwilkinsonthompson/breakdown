9fb85f8187a7e3a960db04d2d37225bbe8961dc7
Daniel Wilkinson-Thompson <daniel@wilkinson-thompson.com>
2023-06-25 22:30:38 +1000

    Added rle4

--- src/compression/rle.c
+++ src/compression/rle.c
@@ -19,150 +19,312 @@ TODO:
 #include <stdbool.h>
 #include "rle.h"
 
-#define MAX_RUN 127
+#define MAX_RLE1_RUN 127
 
-#define BYTE_TO_BINARY(byte)       \
-    (byte & 0x80 ? '1' : '0'),     \
-        (byte & 0x40 ? '1' : '0'), \
-        (byte & 0x20 ? '1' : '0'), \
-        (byte & 0x10 ? '1' : '0'), \
-        (byte & 0x08 ? '1' : '0'), \
-        (byte & 0x04 ? '1' : '0'), \
-        (byte & 0x02 ? '1' : '0'), \
-        (byte & 0x01 ? '1' : '0')
+static uint8_t _nibble_at_index(uint8_t *buffer, uint32_t index);
 
-uint32_t compression_ratio_guess = 5;
-
-uint8_t *rle1_compress(uint8_t *input_buffer, uint32_t input_length, uint32_t *output_length)
+uint8_t *rle1_compress(uint8_t *in, uint32_t count, uint32_t *size)
 {
-    uint8_t *output_buffer;
-    rle1 *current_rle = NULL;
+    uint8_t *out;
+    rle1 *rle = NULL;
     uint8_t run_total = 0;
     uint8_t bit_value;
 
-    // output buffer should be shorter than input buffer, otherwise not compressed
-    output_buffer = (uint8_t *)calloc((size_t)input_length, sizeof(uint8_t));
-    if (output_buffer == NULL)
+    // output buffer should be shorter than input buffer, otherwise not compressed!
+    out = (uint8_t *)calloc((size_t)count * 2, sizeof(uint8_t));
+    if (out == NULL)
         return NULL;
 
-    *output_length = 0;
-    current_rle = (rle1 *)output_buffer; // head of output buffer
-    current_rle->length = 0;
+    *size = 0;
+    rle = (rle1 *)out; // head of output buffer
+    rle->length = 0;
 
-    for (uint32_t buffer_index = 0; buffer_index < input_length; buffer_index++)
+    for (uint32_t byte_ind = 0; byte_ind < count; byte_ind++)
     {
         // if we're starting a new run, set the bit value
-        if (current_rle->length == 0)
+        if (rle->length == 0)
         {
-            current_rle->value = (input_buffer[buffer_index] & 0x80) >> 7; // starting at msb
-            current_rle->length = 0;
+            rle->value = (in[byte_ind] & 0x80) >> 7; // starting at msb
+            rle->length = 0;
         }
 
-        for (uint8_t bit_index = 8; bit_index > 0; bit_index--)
+        for (uint8_t bit_ind = 8; bit_ind > 0; bit_ind--)
         {
-            run_total = current_rle->length;
-            bit_value = (input_buffer[buffer_index] & (0x01 << (bit_index - 1))) >> (bit_index - 1);
+            run_total = rle->length;
+            bit_value = (in[byte_ind] & (0x01 << (bit_ind - 1))) >> (bit_ind - 1);
 
-            if (bit_value == current_rle->value)
+            if (bit_value == rle->value)
             {
                 run_total++;
             }
             else
             {
-                if (*output_length < input_length)
-                {
-                    *output_length += 1;
-                    current_rle++;
-                }
-                else
-                {
-                    *output_length = input_length;
-                    free(output_buffer);
-                    return NULL;
-                }
-                current_rle->value = bit_value;
+                *size += 1;
+                rle++;
+                rle->value = bit_value;
                 run_total = 1;
             }
 
             // deal with overflowing the run length counter
-            if (run_total > MAX_RUN)
+            if (run_total > MAX_RLE1_RUN)
             {
-                current_rle->length = MAX_RUN;
-                if (*output_length < input_length)
-                {
-                    *output_length += 1;
-                    current_rle++;
-                }
-                else
-                {
-                    *output_length = input_length;
-                    free(output_buffer);
-                    return NULL;
-                }
-                current_rle->value = bit_value;
-                current_rle->length = run_total - MAX_RUN;
+                rle->length = MAX_RLE1_RUN;
+                *size += 1;
+                rle++;
+                rle->value = bit_value;
+                rle->length = run_total - MAX_RLE1_RUN;
             }
             else
             {
-                current_rle->length = run_total;
+                rle->length = run_total;
             }
         }
     }
-    *output_length += 1; // include the current_rle in output length
+    *size += 1; // include the rle in output length
 
-    return output_buffer;
+    return out;
 }
 
-uint8_t *rle1_decompress(uint8_t *input_buffer, uint32_t input_length, uint32_t *output_length)
+uint8_t *rle1_decompress(uint8_t *in, uint32_t count, uint32_t *size)
 {
-    uint8_t *output_buffer;
-    uint32_t output_buffer_index;
+    uint8_t *out;
+    uint32_t out_index;
     uint32_t output_length_guess;
+    static uint32_t compression_ratio_guess = 5;
+
     uint8_t partial_byte_bits = 0;
-    rle1 *current_rle;
-    current_rle = (rle1 *)input_buffer;
 
-    // how do we know how large the output might be?
-    output_length_guess = input_length * compression_ratio_guess;
-    output_buffer = (uint8_t *)calloc(sizeof(uint8_t), output_length_guess);
-    if (output_buffer == NULL)
+    rle1 *rle;
+    rle = (rle1 *)in;
+
+    // recursive search for correct output buffer length
+    output_length_guess = count * compression_ratio_guess;
+    out = (uint8_t *)calloc(sizeof(uint8_t), output_length_guess);
+    if (out == NULL)
         return NULL;
-    *output_length = 0;
+    *size = 0;
 
-    for (uint32_t byte_ind = 0; byte_ind < input_length; byte_ind++)
+    for (uint32_t byte_ind = 0; byte_ind < count; byte_ind++)
     {
-        current_rle = (rle1 *)&input_buffer[byte_ind];
-        if (current_rle->value == 1)
+        rle = (rle1 *)&in[byte_ind];
+        if (rle->value == 1)
         {
-            // bug here when current_rle->length = 127??
-            for (uint8_t bit_ind = 0; bit_ind < current_rle->length; bit_ind++)
+            for (uint8_t bit_ind = 0; bit_ind < rle->length; bit_ind++)
             {
-                output_buffer_index = *output_length + bit_ind / 8;
-                output_buffer[output_buffer_index] |= current_rle->value << (7 - ((bit_ind + partial_byte_bits) % 8));
+                out_index = *size + bit_ind / 8;
+                out[out_index] |= rle->value << (7 - ((bit_ind + partial_byte_bits) % 8));
             }
         }
-        partial_byte_bits += (current_rle->length % 8);
+        partial_byte_bits += (rle->length % 8);
         if (partial_byte_bits >= 8)
         {
-            *output_length += partial_byte_bits / 8;
+            *size += partial_byte_bits / 8;
             partial_byte_bits -= 8 * (partial_byte_bits / 8);
         }
-        *output_length += (current_rle->length / 8);
-        if (*output_length >= output_length_guess)
+        *size += (rle->length / 8);
+        if (*size >= output_length_guess)
         {
-            free(output_buffer);
+            free(out);
             // call recursively until buffer is big enough
             compression_ratio_guess *= 1.5;
-            output_buffer = rle1_decompress(input_buffer, input_length, output_length);
+            out = rle1_decompress(in, count, size);
         }
     }
 
-    return output_buffer;
+    return out;
 }
 
-// rle4_compress()
+// uint8_t *rle4_compress(uint8_t *in, uint32_t count, uint32_t *size)
+// {
+//     uint8_t *out;
+//     rle4 *rle;
+//     uint32_t run_count = 0;
+//     uint32_t last_index = 0;
+//     uint8_t upper;
+//     uint8_t lower;
+//     bool match = false;
 
-// rle4_decompress()
+//     out = (uint8_t *)calloc(count, sizeof(uint8_t));
+//     if (out == NULL)
+//         return NULL;
+//     rle = (rle4 *)out;
+
+//     *size = 0;
+//     upper = _nibble_at_index(in, 0);
+//     lower = _nibble_at_index(in, 1);
+//     for (uint32_t index = 0; index < (count * 2); index++)
+//     {
+//         // if (index < (count - 1))
+//         // {
+//         //     fprintf(stderr, "in[%d] = %1x, in[%d] = %1x,\r\n", index, _nibble_at_index(in, index), index + 1, _nibble_at_index(in, index + 1));
+//         //     fprintf(stderr, "upper = %1x, lower = %1x\r\n", upper, lower);
+//         // }
+//         // fprintf(stderr, "nibble = %d\r\n", index);
+//         if (upper == _nibble_at_index(in, index))
+//         {
+//             match = true;
+//             run_count++;
+//             index++;
+//             // fprintf(stderr, "match at nibble %d\r\n", index);
+//             if (lower == _nibble_at_index(in, index))
+//             {
+//                 run_count++;
+//                 index++;
+//                 // fprintf(stderr, "match at nibble %d\r\n", index);
+//             }
+//             else
+//             {
+//                 match = false;
+//             }
+//         }
+//         // fprintf(stderr, "run_count = %d\r\n", run_count);
+
+//         if (!match || (run_count == 0) || (index >= (count - 1)))
+//         {
+//             fprintf(stderr, "match = %c\r\n", match ? '1' : '0');
+//             fprintf(stderr, "run_count = %d\r\n", run_count);
+//             fprintf(stderr, "index = %d vs count = %d\r\n", index, count);
+//             // finish out existing rle
+//             do
+//             {
+//                 rle->upper = upper;
+//                 rle->lower = lower;
+//                 if (run_count >= 256)
+//                 {
+//                     rle->length = 255;
+//                     run_count -= 255;
+//                 }
+//                 else
+//                     rle->length = run_count;
+//                 fprintf(stderr, "out[%d] = %02x %02x\r\n", *size, out[index / 2], out[index / 2 + 1]);
+
+//                 rle++;
+//                 *size += 2;
+//             } while (run_count >= 256);
+
+//             upper = _nibble_at_index(in, index);
+//             lower = _nibble_at_index(in, index + 1);
+//             run_count = 0;
+//         }
+//         match = false;
+//     }
+
+//     return out;
+// }
+
+uint8_t *rle4_compress(uint8_t *in, uint32_t count, uint32_t *size)
+{
+    uint8_t *out;
+    uint32_t run_total;
+    bool odd = false;
+
+    *size = 0;
+    out = (uint8_t *)calloc(count * 2, sizeof(uint8_t));
+    if (out == NULL)
+        return NULL;
+
+    // need to deal with overflowing run length counter
+    // out[*size] = 2;
+    run_total = 2;
+    out[*size + 1] = in[0];
+    for (uint32_t i = 1; i < count; i++)
+    {
+        if (odd == false)
+        {
+            if (in[i] == in[i - 1])
+            {
+                run_total += 2;
+                // out[*size] += 2;
+                continue;
+            }
+            if ((in[i] & 0xF0) == (in[i - 1] & 0xF0))
+            {
+                run_total += 1;
+                // out[*size] += 1;
+                odd = true;
+            }
+            out[*size + 1] = in[i - 1];
+
+            *size += 2;
+            out[*size] = 2;
+        }
+        else
+        {
+            if (i < (count - 1))
+            {
+                if (((in[i] & 0x0F) == (in[i - 1] & 0x0F)) && ((in[i] & 0xF0) == (in[i + 1] & 0xF0)))
+                {
+                    out[*size] += 2;
+                    continue;
+                }
+            }
+            if ((in[i] & 0x0F) == (in[i - 1] & 0x0F))
+            {
+                out[*size] += 1;
+                odd = false;
+            }
+            out[*size + 1] = ((in[i - 1] & 0x0F) << 4) + ((in[i] & 0xF0) >> 4);
+            *size += 2;
+            out[*size] = 2;
+            i++;
+        }
+    }
+
+    *size += 2;
+
+    return out;
+}
+
+uint8_t *rle4_decompress(uint8_t *in, uint32_t size, uint32_t *count)
+{
+    uint8_t *out;
+    uint32_t out_index;
+    uint32_t output_length_guess;
+    static uint32_t compression_ratio_guess = 5;
+
+    uint8_t partial_byte = 0;
+    rle4 *rle;
+    rle = (rle4 *)in;
+
+    // recursive search for correct output buffer length
+    output_length_guess = size * compression_ratio_guess;
+    out = (uint8_t *)calloc(sizeof(uint8_t), output_length_guess);
+    if (out == NULL)
+        return NULL;
+    *count = 0;
+
+    // for (uint32_t byte_ind = 0; byte_ind < count; byte_ind++)
+    // {
+
+    //     rle = (rle4 *)&in[byte_ind];
+    //     for (uint8_t nibble_ind = 0; nibble_ind < rle->length; nibble_ind++)
+    //     {
+    //         out_index = *count + nibble_ind / 2;
+    //         out[out_index] |= rle->value & (0xF0 >> 4 * (nibble_ind) % 2) << (7 - 4 * ((nibble_ind + partial_byte) % 2));
+    //     }
+    //     partial_byte += (rle->length % 2);
+    //     if (partial_byte >= 2)
+    //     {
+    //         *count += 2;
+    //         partial_byte -= 2 * (partial_byte_bits / 8);
+    //     }
+    //     *size += (rle->length / 8);
+    //     if (*size >= output_length_guess)
+    //     {
+    //         free(out);
+    //         // call recursively until buffer is big enough
+    //         compression_ratio_guess *= 1.5;
+    //         out = rle1_decompress(in, count, size);
+    //     }
+    // }
+
+    return out;
+}
+
+static uint8_t _nibble_at_index(uint8_t *buffer, uint32_t index)
+{
+    return ((index % 2) == 0) ? ((buffer[index / 2] & 0xF0) >> 4) : ((buffer[index / 2]) & 0x0F);
+}
 
 // rle8_compress()
 
--- test/compression/rle_test.c
+++ test/compression/rle_test.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "rle.h"
+#include "image.h"
 
 // uint8_t input_buffer[] = {0b00001111, 0b11110000};
 // uint8_t input_buffer[] = {0b11001111, 0b11110000};
@@ -25,13 +26,90 @@ int main(int argc, char *argv[])
 {
     uint8_t *compressed_buffer;
     uint32_t compressed_length;
-    uint8_t *decompressed_buffer;
-    uint32_t decompressed_length;
+    // uint8_t *decompressed_buffer;
+    // uint32_t decompressed_length;
     // uint8_t input_buffer[100] = {0};
-    uint8_t input_buffer[25] = {0b11111111, 0b11111111, 0b11110000, 0b00110000, 0b00101010};
+    // uint8_t input_buffer[25] = {0b11111111, 0b11111111, 0b11110000, 0b00110000, 0b00101010};
+    // uint8_t input_buffer[25] = {0b00001111, 0b11111111, 0b11110000, 0b00110000, 0b00101010};
+    // uint8_t input_buffer[] = {0b11111111, 0b11111111, 0b11110000};
 
-    compressed_buffer = rle1_compress(input_buffer, sizeof(input_buffer), &compressed_length);
+    // compressed_buffer = rle1_compress(input_buffer, sizeof(input_buffer), &compressed_length);
 
+    // fprintf(stderr, "\r\ncompressed_length %d\r\n", compressed_length);
+    // if (compressed_buffer == NULL)
+    // {
+    //     fprintf(stderr, "could not compress buffer\r\n");
+    //     return -1;
+    // }
+    // else
+    //     fprintf(stderr, "compression ratio %lu%%\r\n", 100 * sizeof(input_buffer) / compressed_length);
+
+    // for (uint32_t out_ind = 0; out_ind < compressed_length; out_ind++)
+    // {
+    //     fprintf(stderr, "out[%d] = 0b%c%c%c%c_%c%c%c%c\r\n", out_ind, BYTE_TO_BINARY(compressed_buffer[out_ind]));
+    // }
+
+    // decompressed_buffer = rle1_decompress(compressed_buffer, compressed_length, &decompressed_length);
+
+    // if (decompressed_buffer == NULL)
+    // {
+    //     fprintf(stderr, "could not decompress buffer\r\n");
+    //     return -1;
+    // }
+    // fprintf(stderr, "decompressed length = %d\r\n", decompressed_length);
+
+    // // for (uint32_t out_ind = 0; out_ind < decompressed_length; out_ind++)
+    // // {
+    // //     fprintf(stderr, "out[%d] = 0b%c%c%c%c_%c%c%c%c\r\n", out_ind, BYTE_TO_BINARY(decompressed_buffer[out_ind]));
+    // // }
+
+    // uint32_t mismatched_bytes = 0;
+    // if (decompressed_length != sizeof(input_buffer))
+    //     fprintf(stderr, "decompressed length does not match original buffer length\r\n");
+
+    // else
+    // {
+    //     for (uint32_t ind = 0; ind < decompressed_length; ind++)
+    //     {
+    //         mismatched_bytes += (input_buffer[ind] == decompressed_buffer[ind] ? 0 : 1);
+    //         if (input_buffer[ind] != decompressed_buffer[ind])
+    //             fprintf(stderr, "data[%d]: 0b%c%c%c%c_%c%c%c%c%c= 0b%c%c%c%c_%c%c%c%c\r\n", ind, BYTE_TO_BINARY(input_buffer[ind]), (input_buffer[ind] == decompressed_buffer[ind] ? ' ' : '!'), BYTE_TO_BINARY(decompressed_buffer[ind]));
+    //     }
+    // }
+    // if (mismatched_bytes > 0)
+    // {
+    //     fprintf(stderr, "decompressed data does not match original data: %d mismatched bytes\r\n", mismatched_bytes);
+    // }
+    // else
+    // {
+    //     fprintf(stderr, "decompressed data matches original data\r\n");
+    // }
+
+    // free(compressed_buffer);
+    // free(decompressed_buffer);
+
+    image *test;
+    if (argc > 1)
+        test = image_read(argv[1]);
+    else
+    {
+        fprintf(stderr, "No file specified\r\n");
+        return -1;
+    }
+
+    if (test == NULL)
+        return -1;
+
+    // for (uint32_t row = 0; row < test->height; row++)
+    // {
+    //     for (uint32_t col = 0; col < test->width; col++)
+    //     {
+    //         test->pixel_data[col + row * test->width] = image_argb(255, 0, 255, 255);
+    //     }
+    // }
+
+    compressed_buffer = rle4_compress((uint8_t *)test->pixel_data, test->width * test->height * 4, &compressed_length);
+    fprintf(stderr, "input length %d\r\n", test->width * test->height * 4);
     fprintf(stderr, "compressed_length %d\r\n", compressed_length);
     if (compressed_buffer == NULL)
     {
@@ -39,49 +117,52 @@ int main(int argc, char *argv[])
         return -1;
     }
     else
-        fprintf(stderr, "compression ratio %lu%%\r\n", 100 * sizeof(input_buffer) / compressed_length);
+        fprintf(stderr, "compression ratio %d%%\r\n", 100 * test->width * test->height * 4 / compressed_length);
 
-    for (uint32_t out_ind = 0; out_ind < compressed_length; out_ind++)
+    for (uint32_t out_ind = 0; out_ind < 32; out_ind++)
     {
-        fprintf(stderr, "out[%d] = 0b%c%c%c%c_%c%c%c%c\r\n", out_ind, BYTE_TO_BINARY(compressed_buffer[out_ind]));
+        fprintf(stderr, "out[%d] = %02x %02x\r\n", out_ind, compressed_buffer[out_ind], compressed_buffer[out_ind + 1]);
+        out_ind++;
+        // fprintf(stderr, "out[%d] = 0b%c%c%c%c_%c%c%c%c\r\n", out_ind * 2, BYTE_TO_BINARY(compressed_buffer[out_ind * 2]));
     }
 
-    decompressed_buffer = rle1_decompress(compressed_buffer, compressed_length, &decompressed_length);
+    // decompressed_buffer = rle4_decompress(compressed_buffer, compressed_length, &decompressed_length);
 
-    if (decompressed_buffer == NULL)
-    {
-        fprintf(stderr, "could not decompress buffer\r\n");
-        return -1;
-    }
-    fprintf(stderr, "decompressed length = %d\r\n", decompressed_length);
+    // if (decompressed_buffer == NULL)
+    // {
+    //     fprintf(stderr, "could not decompress buffer\r\n");
+    //     return -1;
+    // }
+    // fprintf(stderr, "decompressed length = %d\r\n", decompressed_length);
 
     // for (uint32_t out_ind = 0; out_ind < decompressed_length; out_ind++)
     // {
     //     fprintf(stderr, "out[%d] = 0b%c%c%c%c_%c%c%c%c\r\n", out_ind, BYTE_TO_BINARY(decompressed_buffer[out_ind]));
     // }
 
-    uint32_t mismatched_bytes = 0;
-    if (decompressed_length != sizeof(input_buffer))
-        fprintf(stderr, "decompressed length does not match original buffer length\r\n");
+    // uint32_t mismatched_bytes = 0;
+    // if (decompressed_length != sizeof(input_buffer))
+    //     fprintf(stderr, "decompressed length does not match original buffer length\r\n");
 
-    else
-    {
-        for (uint32_t ind = 0; ind < decompressed_length; ind++)
-        {
-            mismatched_bytes += (input_buffer[ind] == decompressed_buffer[ind] ? 0 : 1);
-            if (input_buffer[ind] != decompressed_buffer[ind])
-                fprintf(stderr, "data[%d]: 0b%c%c%c%c_%c%c%c%c%c= 0b%c%c%c%c_%c%c%c%c\r\n", ind, BYTE_TO_BINARY(input_buffer[ind]), (input_buffer[ind] == decompressed_buffer[ind] ? ' ' : '!'), BYTE_TO_BINARY(decompressed_buffer[ind]));
-        }
-    }
-    if (mismatched_bytes > 0)
-    {
-        fprintf(stderr, "decompressed data does not match original data: %d mismatched bytes\r\n", mismatched_bytes);
-    }
-    else
-    {
-        fprintf(stderr, "decompressed data matches original data\r\n");
-    }
+    // else
+    // {
+    //     for (uint32_t ind = 0; ind < decompressed_length; ind++)
+    //     {
+    //         mismatched_bytes += (input_buffer[ind] == decompressed_buffer[ind] ? 0 : 1);
+    //         if (input_buffer[ind] != decompressed_buffer[ind])
+    //             fprintf(stderr, "data[%d]: 0b%c%c%c%c_%c%c%c%c%c= 0b%c%c%c%c_%c%c%c%c\r\n", ind, BYTE_TO_BINARY(input_buffer[ind]), (input_buffer[ind] == decompressed_buffer[ind] ? ' ' : '!'), BYTE_TO_BINARY(decompressed_buffer[ind]));
+    //     }
+    // }
+    // if (mismatched_bytes > 0)
+    // {
+    //     fprintf(stderr, "decompressed data does not match original data: %d mismatched bytes\r\n", mismatched_bytes);
+    // }
+    // else
+    // {
+    //     fprintf(stderr, "decompressed data matches original data\r\n");
+    // }
 
     free(compressed_buffer);
+
     return 0;
 }
