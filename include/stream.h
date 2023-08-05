da1ec0593b7cbbad8c5fd370fcc13a56324c5cec
Daniel Wilkinson-Thompson <daniel@wilkinson-thompson.com>
2023-08-06 09:06:08 +1000

    started underflow/overflow handling of streams

--- include/stream.h
+++ include/stream.h
@@ -2,6 +2,7 @@
 #define __stream_h
 
 #include <stdint.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include "error.h"
 #include "buffer.h"
@@ -12,6 +13,11 @@ typedef struct stream_bit_t
     uint8_t bit;
 } stream_bit;
 
+typedef struct stream_t stream;
+
+typedef void (*stream_full_function)(stream *s, size_t size);
+typedef void (*stream_empty_function)(stream *s, size_t size);
+
 typedef struct stream_t
 {
     uint8_t *data;
@@ -19,7 +25,8 @@ typedef struct stream_t
     size_t capacity;
     stream_bit head;
     stream_bit tail;
-    // should add stream empty/full flags and callbacks
+    stream_empty_function empty;
+    stream_full_function full;
 } stream;
 
 // typedef struct stream_t stream;
@@ -40,8 +47,8 @@ uint8_t *stream_read_bytes(stream *s, size_t size, bool reverse_bits);
 buffer *stream_read_buffer(stream *s, size_t size, bool reverse_bits);
 
 // file
-error stream_fwrite(FILE *file, stream *s, bool reverse_bits);
-error stream_fread(FILE *file, stream *s, bool reverse_bits);
+// error stream_fwrite(FILE *file, stream *s, bool reverse_bits);
+// error stream_fread(FILE *file, stream *s, bool reverse_bits);
 
 // debug
 void stream_print(stream *s);
